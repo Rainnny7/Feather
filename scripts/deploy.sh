7b16a93dab0d7b1a78547f25037c79f5efb6ff48
Braydon <xcoolmanxx@gmail.com>
2023-12-12 00:33:45 -0500

    fix(ci): Fix incorrect Maven syntax

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@
 cd "$(dirname "$0")/.." || exit
 
 # Deploy
-mvn deploy -Pgen-javadocs -B -Dstyle.color=always --update-snapshots -T12 -e
+mvn deploy -Pgen-javadocs -B -Dstyle.color=always --update-snapshots -T6C -e
